1df30bb3075be46ec209c79082ddb3f132f4cde2
cakeinpanic <cakeinpanic@gmail.com>
2020-10-10 13:49:27 +0300

    create questionItems json

--- prepare/index.js
+++ prepare/index.js
@@ -1,20 +1,34 @@
 const fs = require('fs')
 const mkdirp = require('mkdirp')
+const path = require('path')
 const { downloadFile, getLocalJSON, getJSON, getFailures } = require('../downloader')
 
 const FOLDER_NAME = 'processed'
 
+const createQuestionItem = (parsedValue, whatPattern, whatPatternTotal) => {
+  let pattern = parsedValue.isTotal ? whatPatternTotal : whatPattern
+  const { value } = parsedValue
+  const what = pattern.replace('{what}', parsedValue.what).replace('{when}', parsedValue.when)
+  return { what, value }
+}
+
 const processOneFile = async (vocabulary) => {
-  const { path } = vocabulary
-  const { vocabulary } = getLocalJSON(`./vocabulary.json`)
+  const { path: filePath } = vocabulary
+  const { values } = getLocalJSON('../' + filePath)
+  return values.map(parsedValue => {
+    return createQuestionItem(parsedValue, vocabulary['what-pattern'], vocabulary['what-pattern-total'])
+  })
 }
 
 const run = async () => {
   const { vocabulary } = getLocalJSON(`./vocabulary.json`)
-  console.log(vocabulary)
+  const questionItems = []
   for (const item of vocabulary) {
-    await processOneFile(item)
+    const oneFile = await processOneFile(item)
+    questionItems.push(...oneFile)
   }
+  console.log(questionItems)
+  fs.writeFileSync(`./questionItems.json`, JSON.stringify({questionItems}))
 }
 
 run()
